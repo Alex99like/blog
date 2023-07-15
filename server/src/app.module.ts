fb0e8fa0a7a1b7602076296cc89a7568ec136fc8
Alexander <aleksievisa@gmail.com>
2023-07-15 23:01:31 +0300

    feat: add db config connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 import { TagModule } from '@app/tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getOrmConfig } from './configs/orm.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TagModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getOrmConfig,
+    }),
+    TagModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,6 @@
 if (!process.env.IS_TS_NODE) {
   require('module-alias/register');
-  console.log('>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<,');
+  console.log('>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<');
   console.log(process.env.IS_TS_NODE);
 }
 
